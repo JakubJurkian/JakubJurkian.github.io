89631889572016e16228ef7036260e3a980b26a1
Jakub <kuba.jur03@gmail.com>
2024-10-23 17:55:21 +0200

    A portolio page hosted by GitHub Pages

---
+++ script.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+  document.getElementById('mobile-menu__bar').classList.toggle('change');
+  document.getElementById('mobile-menu__nav').classList.toggle('change');
+  document.getElementById('mobile-menu-bg').classList.toggle('change-bg');
+}
